30eb4553e3d6dcf1579ecb052552a1da44c6b1e4
MISTI-CAT <yanaisegal200@gmail.com>
2023-09-17 11:18:45 +0300

    Commit

--- src/main/java/net/yanay/morewardentools/MoreWardenToolsDataGenerator.java
+++ src/main/java/net/yanay/morewardentools/MoreWardenToolsDataGenerator.java
@@ -2,17 +2,14 @@ package net.yanay.morewardentools;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.yanay.morewardentools.datagen.ModItemTagProvider;
-import net.yanay.morewardentools.datagen.ModLootTableProvider;
-import net.yanay.morewardentools.datagen.ModModelProvider;
-import net.yanay.morewardentools.datagen.ModRecipeProvider;
+import net.yanay.morewardentools.datagen.*;
 
 public class MoreWardenToolsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModBlockTagProvider::new);
 		pack.addProvider(ModItemTagProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
