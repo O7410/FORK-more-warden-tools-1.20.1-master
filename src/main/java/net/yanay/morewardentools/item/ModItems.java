3d13f2978a67d04fef885635007a5436180fb8d9
MISTI-CAT <yanaisegal200@gmail.com>
2023-09-18 20:45:11 +0300

    Attack Speed change

--- src/main/java/net/yanay/morewardentools/item/ModItems.java
+++ src/main/java/net/yanay/morewardentools/item/ModItems.java
@@ -14,13 +14,13 @@ public class ModItems {
 
 
     public static final Item WARDEN_PICKAXE = registerItem("warden_pickaxe",
-            new PickaxeItem(ModToolsMaterial.WARDEN_HEART, 6, 1f, new FabricItemSettings()));
+            new PickaxeItem(ModToolsMaterial.WARDEN_HEART, 6, -2.7f, new FabricItemSettings()));
     public static final Item WARDEN_AXE = registerItem("warden_axe",
-            new AxeItem(ModToolsMaterial.WARDEN_HEART, 10, 1f, new FabricItemSettings()));
+            new AxeItem(ModToolsMaterial.WARDEN_HEART, 10, -2.5f, new FabricItemSettings()));
     public static final Item WARDEN_SWORD = registerItem("warden_sword",
-            new SwordItem(ModToolsMaterial.WARDEN_HEART, 8, 1f, new FabricItemSettings()));
+            new SwordItem(ModToolsMaterial.WARDEN_HEART, 8, -2f, new FabricItemSettings()));
     public static final Item WARDEN_SHOVEL = registerItem("warden_shovel",
-            new ShovelItem(ModToolsMaterial.WARDEN_HEART, 6, 1f, new FabricItemSettings()));
+            new ShovelItem(ModToolsMaterial.WARDEN_HEART, 6, -3f, new FabricItemSettings()));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(WARDEN_HEART);
